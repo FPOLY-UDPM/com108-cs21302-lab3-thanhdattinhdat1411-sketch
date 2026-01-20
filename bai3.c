169923634b2db8935df3190579473c475132b002
thanhdattinhdat1411-sketch <thanhdattinhdat1411@gmail.com>
2026-01-20 10:44:10 +0700

    Nguyễn Quang Thành Đạt nộp lap 3

--- bai3.c
+++ bai3.c
@@ -17,12 +17,21 @@ int main(){
     scanf("%d", &soDien);
 
     if(soDien <= 50){
-        tienDien = soDien * 1000;
+        tienDien = soDien * 1678;
+    } else if(soDien <= 100){
+        tienDien = 50 * 1678 + (soDien - 50) * 1734;
+    } else if(soDien <= 200){
+        tienDien = 50 * 1678 + 50 * 1734 + (soDien - 100) * 2014;
+    } else if(soDien <= 300){
+        tienDien = 50 * 1678 + 50 * 1734 + 100 * 2014 + (soDien - 200) * 2536;
+    } else if(soDien <= 400){
+        tienDien = 50 * 1678 + 50 * 1734 + 100 * 2014 + 100 * 2536 + (soDien - 300) * 2834;
     } else {
-        tienDien = 50 * 1000 + (soDien - 50) * 1200;
+        tienDien = 50 * 1678 + 50 * 1734 + 100 * 2014 + 100 * 2536 + 100 * 2834 + (soDien - 400) * 2927;
+        
     }
-
-    printf("So tien dien phai dong: %.2f VND\n", tienDien);
+   
+    printf("So tien dien phai dong: %.01f VND\n", tienDien);
 
     return 0;
     
